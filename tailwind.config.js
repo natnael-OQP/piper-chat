039ad1cd65bc58d03b0ea756afc1ecf81caa7bd9
papa-react <makiya112545@gmail.com>
2021-11-21 22:14:43 -1200

    add tailwinf-scrollbar plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   },
   variants: {
     extend: {},
+    scrollbar: ['dark'],
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+    require('tailwind-scrollbar-hide'),
+  ],
 }
