e9c411eea51db6ae53c4e45932d4cee0f493ea05
papa-react <makiya112545@gmail.com>
2021-11-21 22:12:56 -1200

    add image format

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     images: {
-    domains:['seeklogo.com','cdn4.iconfinder.com']
+        domains:['seeklogo.com','cdn4.iconfinder.com','cdn.fakercloud.com'],
+        formats: ['image/avif', 'image/webp'],
+        minimumCacheTTL: 60,
     }
 }
