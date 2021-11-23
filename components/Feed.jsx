13887848512324eea6140cd24e7e9b1e7467909e
papa-react <makiya112545@gmail.com>
2021-11-22 16:03:20 -1200

    styled to overflow to y axis and hide scrollbar

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,17 +4,18 @@ import Posts from './Posts'
 
 const Feed = () => {
     return (
-        <div className="max-w-5xl h-screen mx-auto px-4 flex relative  overflow-y-scroll scrollbar-hide  " >
-            {/* Section */}
-            <section className="flex flex-col mr-4 bg-gray-50 w-full " >
+        <div className=" grid grid-cols-1 gap-4 sm:grid-cols-3 lg:grid-cols-4 max-w-5xl h-full mx-auto px-4 overflow-y-hidden  scrollbar-hide  " >
+            {/* Left */}
+            <section className="col-span-2 lg:col-span-3  mr-4 h-full overflow-y-scroll scrollbar-hide   " >
                 {/* Stories */}
                 <Stories/>
                 {/* post */}
                 <Posts/>
             </section>
-            {/* Section */}
-            <section className="w-96 bg-gray-200 h-96 sticky top-14 " >
+            {/* Right */}
+            <section className="hidden sm:block bg-gray-100 h-96 sticky top-14 " >
                 {/* Mini Profile */}
+                <h1>suggestion</h1>
                 {/* Suggestion */}
             </section>
         </div>
