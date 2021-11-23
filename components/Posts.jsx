cbdc1ecdebddc0df5491a0e1ef7912ce6ae8e18c
papa-react <makiya112545@gmail.com>
2021-11-22 22:04:05 -1200

    create posts component

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Post from './post'
 
 const Posts = () => {
+    const Data = [
+        {
+            id: 1,
+            userName: 'natnael',
+            userImage: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/258_Pied_Piper_logo-512.png',
+            images: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/258_Pied_Piper_logo-512.png',
+            caption:'hello world  '
+        },
+        {
+            id: 2,
+            userName: 'natnael',
+            userImage: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/258_Pied_Piper_logo-512.png',
+            images: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/258_Pied_Piper_logo-512.png',
+            caption:'hello world  '
+        },
+    ]
     return (
-        <div>
-            posts
+        <div className="mt-2" >
+            {
+                Data.map(({ id, ...content }) => (
+                    <Post key={id} {...content} />
+                ))
+            }
         </div>
     )
 }
