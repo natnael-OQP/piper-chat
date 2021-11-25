bc4897d61a7fbc872efa21649065ef2bb3e0e6f1
papa-react <makiya112545@gmail.com>
2021-11-24 21:24:37 -1200

    style and create cmment section

--- components/Post.jsx
+++ components/Post.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {
     BookmarkIcon,
     ChatIcon,
-    DotsHorizontalIcon, HeartIcon, PaperAirplaneIcon,
+    DotsHorizontalIcon, EmojiHappyIcon, HeartIcon, PaperAirplaneIcon,
 
 } from '@heroicons/react/outline'
 
@@ -12,7 +12,7 @@ import {
 
 const Post = ({userName, userImage, images, caption }) => {
     return (
-        <div className=" bg-white py-1 mt-3 rounded-sm shadow-sm " >
+        <div className=" bg-white py-1 mt-3 rounded-sm shadow-sm px-[5px] " >
             {/* Header */}
             <div className="w-full flex items-center justify-between  " >
                 <div className="flex items-center  " >
@@ -26,22 +26,26 @@ const Post = ({userName, userImage, images, caption }) => {
                 <img className="w-full object-cover " src={images} alt={userName} />
             </div>
             {/* Button */}
-            <div className="flex items-center justify-between p-2  " >
-                <div>
+            <div className="w-full flex items-center  justify-between p-2   " >
+                <div className="flex items-center " >
                     <HeartIcon  className="postBtn" />
                     <ChatIcon className="postBtn" />
                     <PaperAirplaneIcon className="postBtn" />
                 </div>
-                <BookmarkIcon/>
+                <BookmarkIcon className="postBtn" />
             </div>
             {/* caption */}
-            <div>
-                <h2>adfkds</h2>
-            </div>
+            <p className="flex items-center p-[6px]  font-semibold text-gray-800 " >
+                {userName+':'} {" "} <span className="text-gray-500 font-normal ml-1 italic ">{caption}</span>
+            </p>
             {/* comment */}
             <div></div>
             {/* Input Filled */}
-            <div></div>
+            <form className="flex items-center p-2 h-10  " >
+                <EmojiHappyIcon className="postBtn w-6 h-6 " />
+                <input className="shadow-sm text-sm font-medium rounded-md ring-green-300 border-none w-full outline-none py-[5px] px-[10px] focus:ring-1 text-gray-500 " type="text" placeholder="write your comment " />
+                <button className="font-semibold text-blue-400 px-1 " type="submit" >post</button>
+            </form>
 
         </div>
     )
