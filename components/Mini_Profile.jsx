42cf3479496c4da6e2098cfe08edf9f658418505
papa-react <makiya112545@gmail.com>
2021-11-26 19:36:03 -1200

    mini-profile component created

---
+++ components/Mini_Profile.jsx
@@ -0,0 +1,19 @@
+
+const Mini_Profile = () => {
+    return (
+        <div className="" >
+            <div className="flex items-center  " >
+                <img
+                    className=" cursor-pointer  w-12 h-12 rounded-full object-cover ring-1 ring-black "
+                    src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/258_Pied_Piper_logo-512.png"
+                    alt='user name' />
+                <div className="ml-4 flex flex-col" >
+                    <h1 className="font-semibold  " >natnael Abebe</h1>
+                    <h1 className=" font-normal text-gray-400 " >Welcome To Piper Chat </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Mini_Profile;
