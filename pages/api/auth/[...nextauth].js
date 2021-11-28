086bfcbfa6b9c38870eb9d2955db954affb48276
papa-react <makiya112545@gmail.com>
2021-11-27 22:37:26 -1200

    next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,24 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
 export default NextAuth({
   // Configure one or more authentication providers
     providers: [
-        GithubProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
-        // ...add more providers here
+
     ],
+    pages: {
+        signIn:'/auth/signin',
+    },
 })
+
+
+// *********************************** default login page ******************************* */
+// theme: {
+//     colorScheme: "auto", // "auto" | "dark" | "light"
+//     brandColor: "#f13287", // Hex color code
+//     logo: "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/258_Pied_Piper_logo-512.png" // Absolute URL to image
+// }
