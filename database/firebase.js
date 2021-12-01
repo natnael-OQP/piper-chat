d4aa479726962ffc99589ed61787987803b7a78a
papa-react <makiya112545@gmail.com>
2021-11-30 21:07:43 -1200

    inital firebase for nextjs app

--- database/firebase.js
+++ database/firebase.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps.length() ?   initializeApp(firebaseConfig) : getApp()
+const app = !getApps().length ?   initializeApp(firebaseConfig) : getApp()
 
 const db = getFirestore();
 const Storage = getStorage();
