91905e2b414943f027375d446c3d43b94de91004
papa-react <makiya112545@gmail.com>
2021-12-01 02:21:58 -1200

    remove providers

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import { getProviders, getSession, useSession } from 'next-auth/react';
 import Head from 'next/head'
 import Feed from '../components/Feed'
-import NaveBar from '../components/Navebar'
-import SignIn from '../components/SignIn';
+import NaveBar from '../components/Navebar';
 
-export default function Home({ providers }) {
+export default function Home() {
   
   // const { data: session } = useSession();
   // if (!session) return <SignIn providers={providers} />;
@@ -16,21 +15,10 @@ export default function Home({ providers }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Header component */}
-      <NaveBar />
+      <NaveBar  />
       {/* Feed component */}
       <Feed/>
     </div>
   )
 }
 
-export async function getServerSideProps(context) {
-    const providers = await getProviders();
-    const session = await getSession(context);
-
-    return {
-      props: {
-        providers,
-        session
-      },
-    };
-}
