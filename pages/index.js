1876bc002dda35bfd2726d8afffa59c98f49c5f5
papa-react <makiya112545@gmail.com>
2021-12-01 06:44:39 -1200

    import model component

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
 import { getProviders, getSession, useSession } from 'next-auth/react';
 import Head from 'next/head'
 import Feed from '../components/Feed'
+import Model from '../components/Model';
 import NaveBar from '../components/Navebar';
 
 export default function Home() {
   
-  // const { data: session } = useSession();
+  const { data: session } = useSession();
   // if (!session) return <SignIn providers={providers} />;
   
   return (
     <div className="scrollbar-hide h-screen overflow-y-scroll bg-gray-50 " >
       <Head>
-        <title>Create Next App</title>
+        <title>hi❤️ {session?.user?.name}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Header component */}
       <NaveBar  />
       {/* Feed component */}
-      <Feed/>
+      <Feed />
+      {/* Model */}
+      <Model />
+      
     </div>
   )
 }
