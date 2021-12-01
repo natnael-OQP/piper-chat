0aaf49a9841832003e1ceff637e9597903f421f3
papa-react <makiya112545@gmail.com>
2021-11-30 21:12:06 -1200

    check the use is loged in or not

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import { getProviders, getSession, useSession } from 'next-auth/react';
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import NaveBar from '../components/Navebar'
+import SignIn from '../components/SignIn';
 
-export default function Home() {
+export default function Home({ providers }) {
+  
+  const { data: session } = useSession();
+  if (!session) return <SignIn providers={providers} />;
+  
   return (
     <div className="scrollbar-hide h-screen overflow-y-scroll bg-gray-50 " >
       <Head>
@@ -15,4 +21,16 @@ export default function Home() {
       <Feed/>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+    const providers = await getProviders();
+    const session = await getSession(context);
+
+    return {
+      props: {
+        providers,
+        session
+      },
+    };
 }
