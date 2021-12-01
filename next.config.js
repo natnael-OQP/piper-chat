2ee87641c84750b5c611d0d954d66e9d2a2159d4
papa-react <makiya112545@gmail.com>
2021-11-30 20:57:38 -1200

    add w3 image domans

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
-        domains:['seeklogo.com','cdn4.iconfinder.com','cdn.fakercloud.com'],
+        domains:['seeklogo.com','cdn4.iconfinder.com','cdn.fakercloud.com','www.w3.org'],
         formats: ['image/avif', 'image/webp'],
         minimumCacheTTL: 60,
     }
