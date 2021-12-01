08a8015d46f03d0ad986698774114106a4fc34e0
papa-react <makiya112545@gmail.com>
2021-12-01 06:27:24 -1200

    to use recoil we wrapp the component by RecoilRoot

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import '../style/global.css'
 import { SessionProvider } from "next-auth/react"
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
+
 
 export default function App({ Component, pageProps: { session, ...pageProps },}) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <RecoilRoot> 
+        <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   )
 }
