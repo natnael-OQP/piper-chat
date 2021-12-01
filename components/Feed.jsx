5edae8d033de9237c8c95c2f06d3added6673492
papa-react <makiya112545@gmail.com>
2021-12-01 02:20:24 -1200

    change max-width for non auth user

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -9,7 +9,8 @@ const Feed = () => {
     const { data: session } = useSession();
     
     return (
-        <div className="max-w-md grid grid-cols-1 sm:gap-4 md:grid-cols-3 lg:grid-cols-3 md:max-w-3xl  lg:max-w-4xl h-full mx-auto px-4 overflow-y-hidden  scrollbar-hide  " >
+        <div
+            className={`max-w-md grid grid-cols-1 sm:gap-4 md:grid-cols-3 lg:grid-cols-3 md:max-w-3xl  lg:max-w-4xl h-full mx-auto px-4 overflow-y-hidden  scrollbar-hide ${!session && "!grid-cols-1 !max-w-xl " } `} >
             {/* Left */}
             <section className=" md:col-span-2 lg:col-span-2  mr-4 h-full overflow-y-scroll scrollbar-hide   " >
                 {/* Stories */}
