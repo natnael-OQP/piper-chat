902a267a255b53e1286ddff32de2d8072ab0b798
papa-react <makiya112545@gmail.com>
2021-11-30 21:41:03 -1200

    show only auth user

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,8 +3,11 @@ import Stories from './Stories'
 import Posts from './Posts'
 import Mini_Profile from './Mini_Profile'
 import Suggestion from './Suggestion'
+import { useSession } from 'next-auth/react'
 
 const Feed = () => {
+    const { data: session } = useSession();
+    
     return (
         <div className="max-w-md grid grid-cols-1 sm:gap-4 md:grid-cols-3 lg:grid-cols-3 md:max-w-3xl  lg:max-w-4xl h-full mx-auto px-4 overflow-y-hidden  scrollbar-hide  " >
             {/* Left */}
@@ -15,12 +18,16 @@ const Feed = () => {
                 <Posts/>
             </section>
             {/* Right */}
-            <section className="hidden md:block  h-96 sticky top-14 " >
-                {/* Mini Profile */}
-                <Mini_Profile/>
-                {/* Suggestion */}
-                <Suggestion/>
-            </section>
+            {
+                session && (
+                    <section className="hidden md:block  h-96 sticky top-14 " >
+                        {/* Mini Profile */}
+                        <Mini_Profile/>
+                        {/* Suggestion */}
+                        <Suggestion/>
+                    </section>
+                )
+            }
         </div>
     )
 }
