3581a42e15adceaacf6a5922c38883a8c2693ab3
papa-react <makiya112545@gmail.com>
2021-11-30 21:16:32 -1200

    get auth use name

--- components/Mini_Profile.jsx
+++ components/Mini_Profile.jsx
@@ -1,6 +1,6 @@
 import { useSession } from 'next-auth/react';
 const Mini_Profile = () => {
-    
+    const {data:session} = useSession()
     return (
         <div className="" >
             <div className="flex items-center  " >
@@ -9,7 +9,7 @@ const Mini_Profile = () => {
                     src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/258_Pied_Piper_logo-512.png"
                     alt='user name' />
                 <div className="ml-4 flex flex-col" >
-                    <h1 className="font-semibold  " >natnael Abebe</h1>
+                    <h1 className="font-semibold  " >{session.user.name}</h1>
                     <h1 className=" font-normal text-gray-400 " >Welcome To Piper Chat </h1>
                 </div>
             </div>
