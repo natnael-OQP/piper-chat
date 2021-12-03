1ff9ea05e90d9c38745af1abcc5e921bc89e6b36
papa-react <makiya112545@gmail.com>
2021-12-03 07:09:17 -1200

    upload post to storage and get image and updat the firestor

--- components/Model.jsx
+++ components/Model.jsx
@@ -1,19 +1,30 @@
 import { Fragment, useRef, useState } from "react";
-//  recoil
+// atoms
 import { modelState } from "../atoms/modelAtom"
 // headless ui
 import { Dialog,Transition  } from '@headlessui/react'
 import { useRecoilState } from "recoil";
-
-import {
-    CameraIcon, XIcon
-} from '@heroicons/react/outline';
+// Icons
+import { CameraIcon, XIcon } from '@heroicons/react/outline';
+// firebase
+import { db, storage } from '../database/firebase';
+// fireStore
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from "@firebase/firestore";
+// next-auth
+import { useSession } from "next-auth/react";
+import { getDownloadURL, ref, uploadString } from "@firebase/storage";
 
 const Model = () => {
-    const [open, setOpen] = useRecoilState(modelState);
+    // auth data
+    const {data:session} = useSession();
+    
+    let [open, setOpen] = useRecoilState(modelState);
     const filePickerRef = useRef(null);
-    const [selectedFile,setSelectedFile] = useState(null)
+    const [selectedFile, setSelectedFile] = useState(null)
+    let [loading, setLoading] = useState(false)
+    const captionRef = useRef(null);
 
+    // select image
     const addImageToPost = (e) => {
         const reader = new FileReader();
         //  read the file 
@@ -24,7 +35,38 @@ const Model = () => {
         reader.onload = (readerEvent) => {
             setSelectedFile(readerEvent.target.result);
         }
+    }
 
+    // upload post
+    const uploadPost = async () => {
+        if (loading) return;
+        setLoading(true);
+        
+        if (captionRef.current.value) {
+            // upload data to firestore
+            const docRef = await addDoc(collection(db, 'posts'), {
+                username: session.user.name,
+                profilePic: session.user.image,
+                caption: captionRef.current.value,
+                timeStamp: serverTimestamp(),
+            })
+            // upload to image to storage
+            const imageRef = ref(storage, `posts/${docRef.id}/image`);
+            await uploadString(imageRef, selectedFile, "data_url").then(async snapshot => {
+                const downloadURL = await getDownloadURL(imageRef);
+                await updateDoc(doc(db, 'posts', docRef.id), {
+                    image:downloadURL 
+                })
+            })
+            
+            // close the model
+            setOpen(false);
+            setLoading(false);
+            setSelectedFile(null);
+        }
+
+        setLoading(false);
+        
     }
 
     return (
@@ -105,7 +147,7 @@ const Model = () => {
                                 </div>
                                 <div className="mt-2" >
                                     <input
-                                        
+                                        ref={captionRef}
                                         type="text"
                                         className="   border-gray-600   px-2 py-1 shadow-md rounded-md focus:ring-[1px]  w-full text-center "
                                         placeholder="Write a caption... "
@@ -117,10 +159,12 @@ const Model = () => {
                         {/* button */}
                         <div className="mt-5 sm:mt-6" >
                             <button
+                                onClick={uploadPost}
+                                disabled={!selectedFile}    
                                 type="button"
-                                className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-gray-50 font-bold  bg-green-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-green-400 "
+                                className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-gray-50 font-bold  bg-green-500 sm:text-sm disabled:bg-gray-400 disabled:cursor-not-allowed hover:bg-gray-300 hover:disabled:bg-green-400 "
                             >
-                                Upload a post
+                                {loading?"Uploading...":"Upload a post"}
                             </button>
                         </div>
                         
