9bab78f289fce4efa8e1dfe01f971b0ed023e2eb
papa-react <makiya112545@gmail.com>
2021-12-03 07:08:04 -1200

    set up new image doman

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
-        domains:['seeklogo.com','cdn4.iconfinder.com','cdn.fakercloud.com','www.w3.org'],
+        domains:['seeklogo.com','cdn4.iconfinder.com','cdn.fakercloud.com','www.w3.org','firebasestorage.googleapis.com'],
         formats: ['image/avif', 'image/webp'],
         minimumCacheTTL: 60,
     }
