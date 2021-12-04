edfb530d57b001608857b7b4018fe29d719b86df
papa-react <makiya112545@gmail.com>
2021-12-04 07:05:39 -1200

    remove firebase.storage domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     images: {
-        domains:['seeklogo.com','cdn4.iconfinder.com','cdn.fakercloud.com','www.w3.org','firebasestorage.googleapis.com'],
+        domains: [
+            'seeklogo.com',
+            'cdn4.iconfinder.com',
+            'cdn.fakercloud.com',
+            'www.w3.org',
+        ],
         formats: ['image/avif', 'image/webp'],
         minimumCacheTTL: 60,
     }
