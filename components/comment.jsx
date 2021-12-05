d9ab80f178d2235d7c5f1fe35524a8328cb70830
papa-react <makiya112545@gmail.com>
2021-12-05 01:14:16 -1200

    remove momentjs

--- components/comment.jsx
+++ components/comment.jsx
@@ -1,5 +1,4 @@
-import React, { forwardRef } from 'react';
-import Moment from 'react-moment';
+import  { forwardRef } from 'react';
 
 const Comment = forwardRef(({ profilePic, username, message, timeStamp },ref) => {
     return (
@@ -7,9 +6,6 @@ const Comment = forwardRef(({ profilePic, username, message, timeStamp },ref) =>
             <div className="flex items-center w-full relative  " >
                 <img className=" m-1 w-8 h-8 rounded-full object-contain ring-1 ring-black " src={profilePic} alt={username}/>
                 <h1 className="ml-2 text-sm font-bold text-gray-600 " >{username}</h1>
-                <span className="absolute top-2 right-2 text-gray-400 text-xs font-semibold ">
-                    <Moment fromNow >{timeStamp?.toDate()}</Moment>
-                </span>
             </div>
             <p className="w-full pb-1 pl-4 pr-[6px] pt-1 text-xs font-semibold text-gray-500 text-left "  >{message}</p>
         </div>
